aecfecd16b9be198787a1930cc0128f4c2599868
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-02-27 14:34:20 -0800

    Second product route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def first_product
+    @product = Product.first
+    render :show
+  end
+
+  def second_product
+    @product = Product.second
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/all_products", controller: "products", action: "all_products"
+  get "/first_product", controller: "products", action: "first_product"
+  get "/second_product", controller: "products", action: "second_product"
 
   # Defines the root path route ("/")
   # root "posts#index"
