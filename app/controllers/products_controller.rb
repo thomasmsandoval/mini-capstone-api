8d82b80b57c312294a813377a5784ddeaed36695
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-02-28 13:33:25 -0800

    Mini capstone update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,10 +26,21 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(
-      name: "X-Chair",
-      price: 399,
-      image_url: "https://www.xchair.com/collections/office-chairs/products/xproject-office-chair?XProject-Textile=XProject.UC01&XProject-FrameFinish=XProject.FC1&XProject-Headrest=XProject.HR0&XProject-SeatSize=XProject.SW1&XProject-SeatFoam=XProject.F2&XProject-Mechanism=XProject.F&XProject-ArmType=XProject.AR4&XProject-BaseType=XProject.BT1&XProject-BaseColor=XProject.BC1&XProject-CylinderHeight=XProject.CH1&XProject-CasterType=*XProject.STD&XProject_STD-Casters=XProject.CS6&XProject-Packaging=KD&campaignid=20590349432&adgroupid=&creative=&matchtype=&network=x&device=c&keyword=&gad_source=1&gclid=CjwKCAiArfauBhApEiwAeoB7qDy_yU7_YUFQ8DKP3u19--FHA723x9uCyEYNYiPSxH7WBr-wbmYxYBoC11QQAvD_BwE",
-      description: "Chair",
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description,
     )
     render :show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/products", controller: "products", action: "index"
   get "/products/:id", controller: "products", action: "show"
   post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
 
   # Defines the root path route ("/")
   # root "posts#index"
