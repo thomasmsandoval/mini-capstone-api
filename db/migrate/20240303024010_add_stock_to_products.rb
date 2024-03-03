277bc9b208f46aa74c57b233e3f7fce16a94122b
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-02 19:17:43 -0800

    Add quantity

--- db/migrate/20240303024010_add_stock_to_products.rb
+++ db/migrate/20240303024010_add_stock_to_products.rb
@@ -1,5 +1,6 @@
 class AddStockToProducts < ActiveRecord::Migration[7.1]
   def change
     add_column :products, :Stock, :integer
+    remove_column :products, :Stock, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_03_024914) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_031215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,10 +18,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_024914) do
     t.string "name"
     t.decimal "price", precision: 10, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "Stock"
+    t.integer "quantity"
   end
 
 end
