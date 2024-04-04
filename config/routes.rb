63a924330d786808574b42dc59647720544a1415
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-04-04 14:01:57 -0700

    worked on issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,20 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "/all_products" => "products#all_products"
-  get "/first_product" => "products#first_product"
-  get "/second_product" => "products#second_product"
   get "/products" => "products#index"
-  get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  get "/products/:id" => "products#show"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
-  get "/all_suppliers" => "suppliers#all_suppliers"
-  get "/suppliers" => "suppliers#index"
-  post "/suppliers" => "suppliers#create"
-  patch "/suppliers/:id" => "suppliers#update"
-  delete "/suppliers/:id" => "suppliers#destroy"
+
   post "/users" => "users#create"
+
   post "/sessions" => "sessions#create"
+
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
   get "/orders" => "orders#index"
+
   post "/carted_products" => "carted_products#create"
   get "/carted_products" => "carted_products#index"
   delete "/carted_products/:id" => "carted_products#destroy"
